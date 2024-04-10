8d7067446e081dbd513662398d9b4d892b167084
AleVic2023 <163424254+AleVic2023@users.noreply.github.com>
2024-04-10 09:56:11 -0400

    Update app1.py
    Ameilloration de code

--- app1.py
+++ app1.py
@@ -1,6 +1,6 @@
 import flask_login
 from flask import Flask, request,render_template,redirect,url_for
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required,current_user
 from flask import flash
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = '@l3V1c2024*'
 
 
-login_manager = flask_login.LoginManager()
+login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
@@ -23,7 +23,7 @@ users = {
     'calixto0721': {'mot_de_passe':generate_password_hash('motdepasse4') ,'acces': 'total'},
 }
 
-#Classe Utilisateur por Flas-Login
+#Class Utilisateur por Flas-Login
 class Utilisateur(UserMixin):
     pass
 
@@ -35,12 +35,10 @@ def charger_utilisateur(identifiant):
         return utilisateur
     return None
 
-@app.route('/')
-def accueil():
-    return render_template('accueil.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         utilisateur = request.form['utilisateur']
@@ -52,37 +50,42 @@ def login():
             login_user(utilisateur_obj)
             acces = users[utilisateur]['acces']
             if acces == 'total':
-                return redirect(url_for(total_acces))
+                return  render_template('accueil_total_acces.html')
             elif acces == 'lecture':
-                return redirect(url_for('lecture_acces'))
+                return  render_template('lecture.html')
         else:
             flash('Identifiant ou mot de passe incorrect', 'error')
 
     return render_template('login.html')
 
 
+@app.route('/lecture')
+@login_required
+def lecture():
+    clients = [
+        'Client1: Nom: Achury, Prenom: Angelica, Courriel: aachury@gmail.com',
+        'Client2: Nom: Langlais, Prenom: Alain, Courriel: alainlanglois15@hotmail.com',
+        'Client3: Nom: Pauline, Prenom: Bellanda, Courriel: bpaul@hotmail.com',
+        'Client4: Nom: Gagnon, Prenom: Benoit,Courriel: gbenoit@gmail.com'
+    ]
+    return render_template("lecture.html",
+                           clients= clients)
+
+
+@app.route('/accueil_total_acces')
+@login_required
+def accueil_total_acces():
+    return render_template('accueil_total_acces.html')
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('accueil'))
+    flash('Vous avez ete deconnecte avec succes')
+    return render_template('login.html')
 
-
-
-@app.route('/total_acces')
-@login_required
-def total_acces():
-    return "Acces total"
-
-@app.route('/lecture_acces')
-@login_required
-def lecture_acces():
-    return "Acces en lecture seule"
-
-@login_manager.unauthorized_handler
-def non_autorise():
-    return 'Veuillez Vous connecter pour accéder à cette page.'
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
